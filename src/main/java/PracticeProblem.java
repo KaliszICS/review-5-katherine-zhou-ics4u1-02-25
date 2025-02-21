837df8c8ff2ff9a334af947395c681fab8587e65
katherine-zhou-ics4u1-02-25 <katherine.zhou3@student.tdsb.on.ca>
2025-02-21 00:36:12 +0000

    Update PracticeProblem

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -67,37 +67,91 @@ public class PracticeProblem {
 
 	public static void q5() {
 		//Write question 5 code here
+		Scanner input = new Scanner(System.in);
+		System.out.print("Input a number: ");
+		int g = input.nextInt();
+		input.nextLine();
+		System.out.print("Input another number: ");
+		int h = input.nextInt();
+		input.nextLine();
+		System.out.print("Input one more number: ");
+		int i = input.nextInt();
+		input.nextLine();
+		System.out.println(Math.max(g, h, i));
+		System.out.println(Math.min (g,h,i));
+
+		
 		
 	}
 	public static void q6() {
 		//Write question 5 code here
+		Scanner input = new Scanner(System.in);
+		System.out.print("Input a sentence: ");
+		String sent = input.nextLine();
+		System.out.println(sent.contains("on"));
 		
 	}
 	public static void q7() {
 		//Write question 5 code here
+		Scanner input = new Scanner(System.in);
+		System.out.print("Input the word mango: ");
+		String man = input.nextLine();
+		System.out.println(man.equalsIgnoreCase("mango"));
 		
 	}
 	public static void q8() {
 		//Write question 5 code here
+		Scanner input = new Scanner(System.in);
+		System.out.print("Input a word: ");
+		String word = input.nextLine();
+		System.out.print("Input a letter: ");
+		int ltr = input.nextLine();
+		System.out.println(word.startsWith(ltr));
+		System.out.println(word.endsWith(ltr));
+
 		
 	}
 
 	public static void q9() {
 		//Write question 5 code here
+		Scanner input = new Scanner(System.in);
+		System.out.print("Input a sentence: ");
+		String len = input.nextLine();
+		System.out.println("Your sentence is " + len.length() + " characters long");
+		
 		
 	}
 	public static void q10() {
 		//Write question 5 code here
+		Scanner input = new Scanner(System.in);
+		System.out.print("Input a sentence: ");
+		String uno = input.nextLine();
+		System.out.print("Input a word to replace: ");
+		String dos = input.nextLine();
+		System.out.print("What word would you like to replace it with: ");
+		String tres = input.nextLine();
+		System.out.println(uno.replaceAll(dos, tres));
+
 		
 	}
 
 	public static void q11() {
 		//Write question 5 code here
+		Scanner input = new Scanner(System.in);
+		System.out.print("Input a sentence: ");
+		String oui = input.nextLine();
+		System.out.println(oui.toUpperCase(oui.trim()));
+		System.out.println(oui.toLowerCase(oui.trim()));
 		
 	}
 
 	public static void q12() {
 		//Write question 5 code here
+		Scanner input = new Scanner(System.in);
+		System.out.print("Input a word: ");
+		String lets = input.nextLine();
+		System.out.println(lets.substring(0, 3));
+		System.out.println(lets.substring(lets.substring() -4));
 		
 	}
 	
